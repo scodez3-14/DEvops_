34e80e8b43e8dda8460006d1ac534669ffdce62a
scodez3-14 <santupii3.1@gmail.com>
2025-09-07 01:20:47 +0530

    fixed conf

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,6 @@ app.get('/api/name', (req, res) => {
   res.json({ name: 'Santu Das' });
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Backend listening on port ${PORT}`);
 });
